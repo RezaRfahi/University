91559c26b8fd5feb07df3135bd9387ae67e2b71d
RezaRfahi <refahireza82@gmail.com>
2022-12-08 22:41:12 +0330

    create log model with all

---
+++ Education/app/Http/Controllers/LogController.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\StoreLogRequest;
+use App\Http\Requests\UpdateLogRequest;
+use App\Models\Log;
+
+class LogController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \App\Http\Requests\StoreLogRequest  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(StoreLogRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Log  $log
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Log $log)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Log  $log
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Log $log)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \App\Http\Requests\UpdateLogRequest  $request
+     * @param  \App\Models\Log  $log
+     * @return \Illuminate\Http\Response
+     */
+    public function update(UpdateLogRequest $request, Log $log)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Log  $log
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Log $log)
+    {
+        //
+    }
+}
---
+++ Education/database/migrations/2022_12_08_190207_create_logs_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('logs', function (Blueprint $table) {
+            $table->id();
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->string('action');
+            $table->string('action_on');
+            $table->string('dsc');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('logs');
+    }
+};
