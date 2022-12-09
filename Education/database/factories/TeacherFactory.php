bfcfb8634082c1535ca0dd5dff7040b24104aecb
RezaRfahi <refahireza82@gmail.com>
2022-12-09 17:15:18 +0330

    set user and teache Factory

--- Education/database/factories/TeacherFactory.php
+++ Education/database/factories/TeacherFactory.php
@@ -2,6 +2,7 @@
 
 namespace Database\Factories;
 
+use Faker\Provider\PhoneNumber;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 /**
@@ -17,7 +18,9 @@ class TeacherFactory extends Factory
     public function definition()
     {
         return [
-            //
+            'name' => $this->faker->name(),
+            'phone_number' => PhoneNumber::numerify('###########'),
+            'email' => $this->faker->unique()->safeEmail()
         ];
     }
 }
--- Education/database/factories/UserFactory.php
+++ Education/database/factories/UserFactory.php
@@ -4,9 +4,12 @@ namespace Database\Factories;
 
 use App\Models\Team;
 use App\Models\User;
+use Faker\Provider\PhoneNumber;
 use Illuminate\Database\Eloquent\Factories\Factory;
 use Illuminate\Support\Str;
 use Laravel\Jetstream\Features;
+use UserPositionEnum as Position;
+use UserLevelEnum as Level;
 
 class UserFactory extends Factory
 {
@@ -24,10 +27,18 @@ class UserFactory extends Factory
      */
     public function definition()
     {
+
         return [
             'name' => $this->faker->name(),
             'email' => $this->faker->unique()->safeEmail(),
             'email_verified_at' => now(),
+            'phone_number' => PhoneNumber::numerify('###########'),
+            'position' => $this->faker->randomElement([Position::assistant, Position::employee,
+                Position::impresario, Position::manager, Position::warden
+            ]),
+            'level' => $this->faker->randomElement([Level::administrator, Level::user,
+                Level::controller, Level::reader
+                ]),
             'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
             'remember_token' => Str::random(10),
         ];
