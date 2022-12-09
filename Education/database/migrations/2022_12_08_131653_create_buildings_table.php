234c98317d04e06cd6644a302997d7bc0c94471f
RezaRfahi <refahireza82@gmail.com>
2022-12-09 19:02:38 +0330

    change forigen key between users and buildings tables

--- Education/database/migrations/2022_12_08_131653_create_buildings_table.php
+++ Education/database/migrations/2022_12_08_131653_create_buildings_table.php
@@ -16,8 +16,6 @@ return new class extends Migration
         Schema::create('buildings', function (Blueprint $table) {
             $table->id();
             $table->string('name')->fulltext();
-            $table->unsignedBigInteger('user_id');
-            $table->foreign('user_id')->references('id')->on('users');
             $table->string('warden_name')->fulltext();
             $table->timestamps();
         });
