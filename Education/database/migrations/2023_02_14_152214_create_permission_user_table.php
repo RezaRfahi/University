07931b2089c009dacb86a024b8b3869bb75ca7dc
RezaRfahi <refahireza82@gmail.com>
2023-02-14 18:56:16 +0330

    make permission and permission_user migration withot edition

---
+++ Education/database/migrations/2023_02_14_152214_create_permission_user_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('permission_user', function (Blueprint $table) {
+            $table->unsignedBigInteger('user_id');
+            $table->unsignedBigInteger('permission_id');
+            $table->timestamps();
+            $table->unique(['user_id', 'permission_id']);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('permission_user');
+    }
+};
