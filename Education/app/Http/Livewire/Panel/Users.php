0650dd64f7ae92ffe315bae816a40e866650d24b
RezaRfahi <refahireza82@gmail.com>
2023-04-01 20:32:47 +0330

    set users table dynomicly

--- Education/app/Http/Livewire/Panel/Users.php
+++ Education/app/Http/Livewire/Panel/Users.php
@@ -2,12 +2,15 @@
 
 namespace App\Http\Livewire\Panel;
 
+use App\Models\User;
 use Livewire\Component;
 
 class Users extends Component
 {
+    public $users;
     public function render()
     {
+        $this->users=User::all()->paginate(5);
         return view('livewire.panel.users')->layout('components.admin.app');
     }
 }
--- Education/resources/views/livewire/panel/users.blade.php
+++ Education/resources/views/livewire/panel/users.blade.php
@@ -31,18 +31,23 @@
             </tr>
             </thead>
             <tbody>
+            @foreach($users as $user)
             <tr>
-                <td><img src="https://via.placeholder.com/30x30" alt="profile image" class="profile-img"></td>
-                <td>Apple</td>
-                <td>Fruit</td>
-                <td>$1.99</td>
-                <td>admin</td>
+                <td><img src="{{ $user->profile_photo_url }}" alt="profile image" class="profile-img"></td>
+                <td>{{$user->name}}</td>
+                <td>{{$user->email}}</td>
+                <td>{{$user->phone_number}}</td>
+                <td>{{$user->position}}</td>
                 <td>
                     <button type="button" class="btn btn-danger btn-sm">حذف</button>
                     <button type="button" class="btn btn-secondary btn-sm">ویرایش</button>
                 </td>
             </tr>
+            @endforeach
             </tbody>
         </table>
+{{--        <div class="d-flex">--}}
+{{--            {!! $users->links() !!}--}}
+{{--        </div>--}}
     </div>
 <!-- /.row (main row) -->
