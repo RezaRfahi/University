dd895b63a8464e1012fadb49fe5062ebbb56358a
RezaRfahi <refahireza82@gmail.com>
2023-04-12 22:19:13 +0330

    add createUser in route

--- Education/routes/web.php
+++ Education/routes/web.php
@@ -4,6 +4,8 @@ use Illuminate\Support\Facades\Route;
 use \App\Http\Controllers\PanelController;
 use \App\Http\Livewire\Panel\Index as PanelIndex;
 use App\Http\Livewire\Panel\Users as PanelUsers;
+use App\Http\Livewire\Panel\Users\CreateUser as PanelCreateUser;
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -23,12 +25,15 @@ Route::middleware([
     'auth:sanctum',
     config('jetstream.auth_session'),
     'verified'
-])->group(function () {
+])->group(callback: function () {
     Route::get('/dashboard', function () {
         return view('dashboard');
     })->name('dashboard');
     Route::prefix('panel')->group(callback: function (){
         Route::get('/', PanelIndex::class)->name('panel.index');
-        Route::get('/users', PanelUsers::class)->name('panel.users');
+        Route::prefix('users')->group(callback: function (){
+            Route::get('/', PanelUsers::class)->name('panel.users');
+            Route::get('/createuser', PanelCreateUser::class)->name('panel.user.create');
+        });
     });
 });
